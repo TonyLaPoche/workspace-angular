8476db4a530c4c408bdf209d8c80e4d897f518b7
tonylapoche <terrade.antoine.pro@gmail.com>
2024-11-19 17:40:36 +0100

    update form template style, add consignes.md and angular-material

--- projects/reactive-forms/src/app/app.component.ts
+++ projects/reactive-forms/src/app/app.component.ts
@@ -1,16 +1,14 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {FormGroupComponent} from "./components/form-groupe/form-group.component";
-import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {FormUser} from "./components/form-user/form-user.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, FormGroupComponent, MatSlideToggle],
+    imports: [RouterOutlet, FormUser],
     template: `
         <h1 class="text-2xl text-center">Reactive Forms</h1>
-        <app-form-group/>
-        <mat-slide-toggle>Toggle me!</mat-slide-toggle>
+        <app-form-user/>
     `,
     styles: ``,
     changeDetection: ChangeDetectionStrategy.OnPush
--- projects/reactive-forms/src/app/components/form-user/form-user.component.ts
+++ projects/reactive-forms/src/app/components/form-user/form-user.component.ts
@@ -1,29 +1,37 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
-    selector: 'app-form-group',
+    selector: 'app-form-user',
     standalone: true,
     imports: [
         ReactiveFormsModule,
+        MatFormField,
+        MatLabel,
+        MatInput,
     ],
-    templateUrl: 'form-group.component.html',
+    templateUrl: 'form-user.component.html',
     styles: ``,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FormGroupComponent {
+export class FormUser {
     registerForm = new FormGroup(
         {
             user: new FormGroup({
                 name: new FormControl(''),
                 lastName: new FormControl(''),
                 email: new FormControl(''),
+                age: new FormControl(''),
+                phoneNumber: new FormControl(''),
+                password: new FormControl(''),
+                passwordConfirm: new FormControl(''),
             }),
             address: new FormGroup({
+                street: new FormControl(''),
                 city: new FormControl(''),
-                state: new FormControl(''),
                 zipcode: new FormControl(''),
-                country: new FormControl(''),
             })
         }
     );
