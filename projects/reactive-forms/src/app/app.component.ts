c16f539b3463633250d7ddf75facd62816ea62c9
tonylapoche <terrade.antoine.pro@gmail.com>
2024-11-19 16:16:32 +0100

    init form

--- projects/reactive-forms/src/app/app.component.ts
+++ projects/reactive-forms/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {FormGroupComponent} from "./components/form-groupe/form-group.component";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, FormGroupComponent],
+    imports: [RouterOutlet, FormGroupComponent, MatSlideToggle],
     template: `
         <h1 class="text-2xl text-center">Reactive Forms</h1>
-        <app-form-groupe/>
+        <app-form-group/>
+        <mat-slide-toggle>Toggle me!</mat-slide-toggle>
     `,
     styles: ``,
     changeDetection: ChangeDetectionStrategy.OnPush
--- projects/reactive-forms/src/app/components/form-groupe/form-group.component.ts
+++ projects/reactive-forms/src/app/components/form-groupe/form-group.component.ts
@@ -1,38 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
-import {FormAdresseComponent} from "./form-adresse/form-adresse.component";
-import {FormUserComponent} from "./form-user/form-user.component";
 
 @Component({
-  selector: 'app-form-groupe',
-  standalone: true,
+    selector: 'app-form-group',
+    standalone: true,
     imports: [
         ReactiveFormsModule,
-        FormAdresseComponent,
-        FormUserComponent
     ],
-  templateUrl: 'form-group.component.html',
-  styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+    templateUrl: 'form-group.component.html',
+    styles: ``,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormGroupComponent {
-  authenticate = new FormGroup(
-      {
-          user: new FormGroup({
-              name: new FormControl(''),
-              lastName: new FormControl(''),
-              email: new FormControl(''),
-          }),
-          address: new FormGroup({
-              city: new FormControl(''),
-              state: new FormControl(''),
-              zipcode: new FormControl(''),
-              country: new FormControl(''),
-          })
-      }
-  );
+    registerForm = new FormGroup(
+        {
+            user: new FormGroup({
+                name: new FormControl(''),
+                lastName: new FormControl(''),
+                email: new FormControl(''),
+            }),
+            address: new FormGroup({
+                city: new FormControl(''),
+                state: new FormControl(''),
+                zipcode: new FormControl(''),
+                country: new FormControl(''),
+            })
+        }
+    );
 
-  submitAuth() {
-    console.info(this.authenticate.value)
-  }
+    submitAuth() {
+        console.info(this.registerForm.value)
+    }
 }
