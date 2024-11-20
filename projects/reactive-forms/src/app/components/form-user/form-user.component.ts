4f637841d6e5d46dbfd05c06afa1f5a0d01b76e4
tonylapoche <terrade.antoine.pro@gmail.com>
2024-11-20 14:17:38 +0100

    update: avancer sur les validators et les messages d'erreur [WIP]

--- projects/reactive-forms/src/app/components/form-user/form-user.component.ts
+++ projects/reactive-forms/src/app/components/form-user/form-user.component.ts
@@ -1,9 +1,12 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
-import {matchValidator, passwordMatchValidator} from "./form-user.validators";
+import {matchValidator} from "./form-user.validators";
 import {JsonPipe} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatOption, MatSelect} from "@angular/material/select";
 
 @Component({
     selector: 'app-form-user',
@@ -15,13 +18,16 @@ import {JsonPipe} from "@angular/common";
         MatInput,
         MatError,
         JsonPipe,
+        MatButton,
+        MatCheckbox,
+        MatSelect,
+        MatOption,
     ],
     templateUrl: 'form-user.component.html',
     styles: ``,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormUser {
-
 
 
     registerForm = new FormGroup(
@@ -33,18 +39,32 @@ export class FormUser {
                 age: new FormControl('', [Validators.required, Validators.min(18), Validators.maxLength(100)]),
                 phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10}$')]),
                 password: new FormControl('', [Validators.required, Validators.minLength(8), matchValidator('passwordConfirm', true)]),
-                passwordConfirm: new FormControl('', [Validators.required,  matchValidator('password')]),
-            }, {validators: passwordMatchValidator}),
+                passwordConfirm: new FormControl('', [Validators.required, matchValidator('password')]),
+            }),
             address: new FormGroup({
                 street: new FormControl('', [Validators.required]),
                 city: new FormControl('', [Validators.required]),
                 zipcode: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{5}$')]),
-            })
+            }),
+            optional: new FormGroup({
+                newsletterSub: new FormControl(''),
+                newsletterFrequency: new FormControl('', Validators.required),
+                website: new FormControl('', Validators.pattern('https?://.+')),
+                birthDate: new FormControl('', Validators.required),
+                gender: new FormControl('', Validators.required),
+                commentary: new FormControl('')
+            }),
+            hobbies: new FormArray([
+                new FormControl('hobby')
+            ])
+
         }
     );
 
     userForm = this.registerForm.controls.user;
     addressForm = this.registerForm.controls.address;
+    optionalForm = this.registerForm.controls.optional;
+    hobbiesForm = this.registerForm.controls.hobbies;
 
     submitAuth() {
         console.info(this.registerForm.value)
