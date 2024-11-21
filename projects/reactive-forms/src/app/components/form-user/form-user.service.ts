07e596b599f38f7c82dfaf166e8acd82fded5ec4
tonylapoche <terrade.antoine.pro@gmail.com>
2024-11-21 16:13:59 +0100

    update: check majority date (greater than 18years old)

--- projects/reactive-forms/src/app/components/form-user/form-user.service.ts
+++ projects/reactive-forms/src/app/components/form-user/form-user.service.ts
@@ -8,20 +8,38 @@ import {AbstractControl} from "@angular/forms";
 export class FormUserService {
 
   checkBirthDateValidity(control: AbstractControl) {
-    const today = new Date()
-    if (control.value.toISOString() < today.toISOString()) {
-      console.log(control.value.toISOString())
-      console.log('---')
-      console.log(today.toISOString())
+    const currentDate = new Date();
+    const inputDate = new Date(control.value);
 
+    const currentYears = currentDate.getFullYear();
+    const currentMonths = currentDate.getMonth();
+    const currentDay = currentDate.getDate();
+
+    const inputDateYears = inputDate.getFullYear();
+    const inputDateMonths = inputDate.getMonth();
+    const inputDateDay = inputDate.getDate();
+
+    const age = currentYears - inputDateYears;
+    const hasHadBirthdayThisYear =
+        currentMonths > inputDateMonths ||
+        (currentMonths === inputDateMonths && currentDay >= inputDateDay);
+
+    if (age < 18 || (age === 18 && hasHadBirthdayThisYear)) {
+      return of({ birthDate: '18 ans révolue exigé' }).pipe(
+          take(1),
+          delay(1000))
+    }
+
+    if (control.value.toISOString() < currentDate.toISOString()) {
         return of(null).pipe(
             take(1),
-            delay(5000)
+            delay(1000)
         )
-    } else {
-      return of({ birthDate: true }).pipe(
+    }
+     else {
+      return of({ birthDate: 'Birth date invalid' }).pipe(
           take(1),
-          delay(5000)
+          delay(1000)
 
       )
     }
